cb36e3e8e96f8c67f30d1e2e45d7369cc171008d
cargueta01 <aa20099@ues.edu.sv>
2023-10-26 17:21:00 -0600

    debug true

--- GameZone/settings.py
+++ GameZone/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(&5c5xsphfay0g_rguh78q-94#o52zjdl)t0^4$%edj!$-z$w_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
